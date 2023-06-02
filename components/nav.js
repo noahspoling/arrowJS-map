19ade0f11808e6b73895670b17b3501e58ff7228
Noah Poling <noahspoling@gmail.com>
2023-06-02 09:25:36 -0400

    Remade the project. It was either using vite with vanillaJS or a webpack

---
+++ components/nav.js
@@ -0,0 +1,17 @@
+import { html, reactive, watch } from '@arrow-js/core'
+export const nav = html`
+    <nav>
+        <ul>
+            <li>
+                <span>Home</span>
+            </li>
+            <li>
+                <span>Map</span>
+            </li>
+            <li>
+                <span>Data</span>
+            </li>
+        </ul>
+    </nav>
+`
+
